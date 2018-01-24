32fd4ff63b2d225d2c1ab27d2d39aafd9fecd0d2
Jacob Amos <jamos125@gmail.com>
2018-01-24 14:59:15 -0500

    stop updating julia 0.5

--- Applications/Julia/update_julia.sh
+++ Applications/Julia/update_julia.sh
@@ -9,20 +9,20 @@ if [[ -z "${CPU}" ]]; then
     CPU=4
 fi
 
-# version 0.5
-cd $JULIA_INSTALL_DIR
-if [ ! -d "0.5" ]; then
-    git clone https://github.com/JuliaLang/julia.git 0.5
-    cd 0.5
-    git fetch --all
-    git checkout release-0.5
-    git pull
-    make -C deps getall && make -j $CPU && ./julia -e "Pkg.update()"
-else
-    cd 0.5
-    git pull
-    make -j $CPU && ./julia -e "Pkg.update()"
-fi
+# # version 0.5
+# cd $JULIA_INSTALL_DIR
+# if [ ! -d "0.5" ]; then
+#     git clone https://github.com/JuliaLang/julia.git 0.5
+#     cd 0.5
+#     git fetch --all
+#     git checkout release-0.5
+#     git pull
+#     make -C deps getall && make -j $CPU && ./julia -e "Pkg.update()"
+# else
+#     cd 0.5
+#     git pull
+#     make -j $CPU && ./julia -e "Pkg.update()"
+# fi
 
 # version 0.6
 cd $JULIA_INSTALL_DIR
