d2214f8a17e6387d6274a10de0613ce014aa80a0
Jacob Amos <jamos125@gmail.com>
2018-04-27 13:16:01 -0400

    ipython configuration file added to config dir

---
+++ config/ipython_config.py
@@ -0,0 +1,14 @@
+# NOTE: this config file should generally be located:
+# ~/.ipython/profile_default/ipython_config.py
+
+import numpy as np
+import pandas as pd
+
+# np.core.arrayprint._line_width = 140
+np.set_printoptions(linewidth=140)
+pd.options.display.max_rows = 40
+pd.set_option('display.max_rows', 40)
+
+config = get_config()
+c.InteractiveShell.confirm_exit = False
+c.InteractiveShell.editor = 'vim'
