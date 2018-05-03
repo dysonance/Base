a308adbe99533545311d4e53aca6fb11e21e04db
Jacob Amos <jamos125@gmail.com>
2018-05-03 12:25:04 -0400

    default ipython lines to execute in config

--- config/ipython_config.py
+++ config/ipython_config.py
@@ -12,3 +12,13 @@ pd.set_option('display.max_rows', 40)
 config = get_config()
 c.InteractiveShell.confirm_exit = False
 c.InteractiveShell.editor = 'vim'
+
+c.InteractiveShellApp.exec_lines = [
+    '%load_ext autoreload',
+    '%autoreload 2',
+    'import numpy as np',
+    'import pandas as pd',
+    'import datetime as dt',
+    'from pylab import *',
+    'ion()',
+]
