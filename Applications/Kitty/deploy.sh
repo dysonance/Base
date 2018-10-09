07a5ea6147b698b03486e3f84e2990d2115d03a4
Jacob Amos <jamos125@gmail.com>
2018-10-09 16:16:54 -0400

    remove unused apps and their configs

--- Applications/Kitty/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-KITTY_INSTALL_DIR=$HOME/Preferences/Applications/Kitty
-
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin \
-    dest=$KITTY_INSTALL_DIR \
-    launch=n
