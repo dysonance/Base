d895d5bd1e817183e42e26f9fb0cec25cc601566
Jacob Amos <jamos125@gmail.com>
2018-12-11 09:47:26 -0500

    stop updating julia pre-1.0 versions

--- Applications/Julia/update.sh
+++ Applications/Julia/update.sh
@@ -9,23 +9,6 @@ if [[ -z "${CPU}" ]]; then
     CPU=4
 fi
 
-# version 0.7 (pre-1.0 version)
-cd $JULIA_INSTALL_DIR
-if [ ! -d "v0.7" ]; then
-    git clone https://github.com/JuliaLang/julia.git v0.7
-    cd v0.7
-    git fetch --all
-    git checkout release-0.7
-    git pull
-    cp ../Make.user .
-    make -C deps getall && make -j $CPU CFLAGS=-Wno-error && ./julia --color=yes --depwarn=no --warn-overwrite=no --optimize=3 --inline=yes -e "using Pkg; Pkg.update(); Pkg.build()"
-else
-    cd v0.7
-    git pull
-    cp ../Make.user .
-    make -j $CPU CFLAGS=-Wno-error && ./julia --color=yes --depwarn=no --warn-overwrite=no --optimize=3 --inline=yes -e "using Pkg; Pkg.update(); Pkg.build()"
-fi
-
 # version 1.0
 cd $JULIA_INSTALL_DIR
 if [ ! -d "v1.0" ]; then
