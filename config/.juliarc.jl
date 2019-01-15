d879b66c89bb332b0afa22f6cdc2155f146625c5
dysonance <jamos125@gmail.com>
2019-01-14 21:42:58 -0500

    edit julia startup config file

--- config/.juliarc.jl
+++ config/.juliarc.jl
@@ -1,20 +1,21 @@
-#  const CUSTOM_PROMPT = ":> "
-#
-#  if VERSION >= v"0.7-"
-#      using REPL
-#      function customize_repl(repl)
-#          repl.interface = REPL.setup_interface(repl)
-#          repl.interface.modes[1].prompt = CUSTOM_PROMPT
-#          return
-#      end
-#      atreplinit(customize_repl)
-#  elseif VERSION >= v"0.6-"
-#      function customize_repl(repl)
-#          repl.interface = Base.REPL.setup_interface(repl)
-#          repl.interface.modes[1].prompt = CUSTOM_PROMPT
-#          return
-#      end
-#      atreplinit(customize_repl)
-#  end
-#
-#
+# julia custom startup file
+# to be placed at: ~/.julia/config/startup.jl
+
+const CUSTOM_PROMPT = ":> "
+
+if VERSION >= v"0.7-"
+    using REPL
+    function customize_repl(repl)
+        repl.interface = REPL.setup_interface(repl)
+        repl.interface.modes[1].prompt = CUSTOM_PROMPT
+        return
+    end
+    atreplinit(customize_repl)
+elseif VERSION >= v"0.6-"
+    function customize_repl(repl)
+        repl.interface = Base.REPL.setup_interface(repl)
+        repl.interface.modes[1].prompt = CUSTOM_PROMPT
+        return
+    end
+    atreplinit(customize_repl)
+end
