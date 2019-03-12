df59b17460f894c33905a5bb815f407c8c1c7576
Jacob Amos <jamos125@gmail.com>
2019-03-12 18:30:54 -0400

    update script fixes

--- tools/update.sh
+++ tools/update.sh
@@ -3,12 +3,16 @@
 # entry
 cd $HOME/Chest
 
+# expand aliases and define them
+shopt -s expand_aliases
+source ~/.bash_profile
+
 # packages
 brew update
 brew upgrade
 
 # python
-ipi install --upgrade $(pip list --format=freeze | sed 's/==/ /' | awk '{print $1}')
+ipi install --upgrade $(ipi list --format=freeze | sed 's/==/ /' | awk '{print $1}')
 
 # alacritty
 ./apps/alacritty/deploy.sh
@@ -20,4 +24,4 @@ py $HOME/.vim/plugged/YouCompleteMe/install.py --all
 
 # julia
 ./apps/julia/deploy.sh
-julia -g2 --color=yes apps/julia/precompile.jl
+julia -O3 apps/julia/precompile.jl
