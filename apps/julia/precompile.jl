c87bbafdb7ca0b533e54cdb1c8bfd8c251e55725
dysonance <jamos125@gmail.com>
2019-03-13 00:43:16 -0400

    use progress bar in precompile script

--- apps/julia/precompile.jl
+++ apps/julia/precompile.jl
@@ -1,9 +1,28 @@
 using Pkg
 
-pkgs = String.(keys(Pkg.installed()))
-
-println("precompiling packages:")
-for (i, pkg) in enumerate(pkgs)
-    println("\t$pkg ($i/$(length(pkgs)))")
+function precompile(pkg::String)
     eval(Meta.parse("using $pkg"))
+end
+
+const PKGS = String.(keys(Pkg.installed()))
+
+if "ProgressMeter" in PKGS
+    using ProgressMeter
+    useprogress = true
+    progress = Progress(length(PKGS), 1, "Precompiling Packages")
+else
+    useprogress = false
+end
+
+for (i, pkg) in enumerate(PKGS)
+    if useprogress
+        next!(progress)
+    else
+        if i==1
+            println("precompiling packages:")
+        end
+        status = "$pkg ($i/$(length(PKGS)))"
+        println("\t$status")
+    end
+    precompile(pkg)
 end
