f08a74885a31002d1eb5c72895999fd901bba978
Jacob Amos <jamos125@gmail.com>
2019-04-08 11:44:30 -0400

    update and clean up environment setup scripts in tools dir

--- tools/setup.sh
+++ tools/setup.sh
@@ -1,50 +1,56 @@
 #!/bin/bash
 
-# install preliminary dependencies
-brew install \
-    make cmake \
-    git wget \
-    python go postgresql markdown pandoc \
-    ctags tmux tree htop lnav \
-    pgformatter shfmt clang-format
+# ensure starting directory and environment setup with aliases
+cd ~/Chest
+shopt -s expand_aliases
+source ~/.bash_profile
 
 # define convenience function to create directory if it does not exist
 function SetupDirectory()
 {
     local _directory_=$1
-    if [[ ! -d "$_directory_" ]]; then
+    if [ "$_directory_" == "" ]; then
+        echo "must pass directory name"
+    elif [[ ! -d "$_directory_" ]]; then
         mkdir $_directory_
     fi
 }
 
-# link personal configuration files
-ln -sf $HOME/Chest/config/.vimrc $HOME/
-ln -sf $HOME/Chest/config/.gitconfig $HOME/
-ln -sf $HOME/Chest/config/.Rprofile $HOME/
-ln -sf $HOME/Chest/config/.tmux.conf $HOME/
-
-# setup vim environment
-cd $HOME/Chest/apps/vim
-./deploy.sh
-SetupDirectory $HOME/.vim
-SetupDirectory $HOME/.vim/colors
-SetupDirectory $HOME/.vim/syntax
-ln -sf $HOME/Chest/.vim/colors/jamos_blue.vim $HOME/.vim/colors/
-ln -sf $HOME/Chest/.vim/syntax/cpp.vim $HOME/.vim/syntax/
-ln -sf $HOME/Chest/.vim/skeletons $HOME/.vim/
-ln -sf $HOME/Chest/.vim/plugins.vim $HOME/.vim/
-for syntax_file in $HOME/Chest/.vim/syntax/*.vim; do
-    echo "linking $syntax_file to $HOME/.vim/syntax/"
-    ln -sf $syntax_file $HOME/.vim/syntax/
-done
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
-vim -c ":PlugInstall | :PlugUpdate | :qa"
-cd $HOME/.vim/plugged/YouCompleteMe
-python3 install.py --all
-cd $HOME/Chest
+# install preliminary dependencies
+./apps/brew/deploy.sh
+brew install $(cat data/brew_list.txt)
 
 # setup python environment
-pip3 install numpy pandas matplotlib ipython numba statsmodels scipy
-SetupDirectory $HOME/.ipython
-SetupDirectory $HOME/.ipython/profile_default
-ln -sf $HOME/Chest/config/ipython_config.py $HOME/.ipython/profile_default/
+./apps/python/deploy.sh
+./apps/python/deploy.sh 3.6.5
+pip36 install data/pip_list.txt
+ipi install data/pip_list.txt
+SetupDirectory ~/.ipython
+SetupDirectory ~/.ipython/profile_default
+ln -sf ~/Chest/config/ipython_config.py ~/.ipython/profile_default/
+
+# setup vim environment
+./apps/vim/deploy.sh
+SetupDirectory ~/.vim
+SetupDirectory ~/.vim/colors
+SetupDirectory ~/.vim/syntax
+ln -sf ~/Chest/.vim/colors/* ~/.vim/colors/
+ln -sf ~/Chest/.vim/syntax/* ~/.vim/syntax/
+ln -sf ~/Chest/.vim/indent/* ~/.vim/indent/
+ln -sf ~/Chest/.vim/filetype.vim ~/.vim/
+ln -sf ~/Chest/.vim/package.vim ~/.vim/
+ln -sf ~/Chest/config/.vimrc ~/.vimrc
+# install vim plugged package manager
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+vim -c ":PlugInstall | :qa"
+
+# setup alacritty terminal
+./apps/alacritty/deploy.sh
+
+# put configuration files in the right places
+SetupDirectory ~/.config
+SetupDirectory ~/.config/alacritty
+ln -sf ~/Chest/config/alacritty.yml ~/
+ln -sf ~/Chest/config/.tmux.conf ~/
+ln -sf ~/Chest/config/.Rprofile ~/
+ln -sf ~/Chest/config/.psqlrc ~/
