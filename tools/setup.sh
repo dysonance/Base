5a45b13e06a6a4b82622f59293c468424e428d9f
Jacob Amos <jamos125@gmail.com>
2019-10-02 19:18:55 -0400

    improve environment setup script

--- tools/setup.sh
+++ tools/setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# stop and exit script at first error
+set -e
+
 # ensure starting directory and environment setup with aliases
 cd ~/Chest
 shopt -s expand_aliases
@@ -12,14 +15,19 @@ function SetupDirectory()
     if [ "$_directory_" == "" ]; then
         echo "must pass directory name"
     elif [[ ! -d "$_directory_" ]]; then
+        echo "setting up directory $_directory_"
         mkdir $_directory_
     fi
 }
 
 # setup environment configurations/preferences
+echo "setting up environment directories"
 SetupDirectory ~/.vim
 SetupDirectory ~/.config
 SetupDirectory ~/.config/alacritty
+SetupDirectory ~/.ipython
+SetupDirectory ~/.ipython/profile_default
+echo "linking configuration files"
 ln -sf ~/Chest/.vim/colors ~/.vim/colors
 ln -sf ~/Chest/.vim/syntax ~/.vim/syntax
 ln -sf ~/Chest/.vim/indent ~/.vim/indent
@@ -30,29 +38,38 @@ ln -sf ~/Chest/config/alacritty.yml ~/
 ln -sf ~/Chest/config/.tmux.conf ~/
 ln -sf ~/Chest/config/.Rprofile ~/
 ln -sf ~/Chest/config/.psqlrc ~/
+ln -sf ~/Chest/config/ipython_config.py ~/.ipython/profile_default/
 
 # install preliminary dependencies
 if [ "$(which brew)" == "" ]; then
+    echo "installing homebrew"
     ./apps/brew/deploy.sh
 fi
-brew install $(cat data/brew_list.txt | sed "s/,.*$//g")
+echo "installing brew packages"
+brew install htop tmux reattach-to-user-namespace
+brew install $(cat data/brew_packages.csv | sed "s/,.*$//g" | grep -v "package")
+
+# setup alacritty terminal
+echo "installing alacritty"
+./apps/alacritty/deploy.sh
 
 # setup llvm environment (required for later python 3 version configurations)
+echo "installing llvm toolset"
 ./apps/llvm/deploy.sh
 
 # setup python environment (required for vim and compatibility with other tech)
+echo "installting python (latest)"
 ./apps/python/deploy.sh
+echo "installing python (3.6.5)"
 ./apps/python/deploy.sh 3.6.5
-pip36 install data/pip_list.txt
-ipi install data/pip_list.txt
-SetupDirectory ~/.ipython
-SetupDirectory ~/.ipython/profile_default
-ln -sf ~/Chest/config/ipython_config.py ~/.ipython/profile_default/
+echo "installing python packages"
+pip36 install $(cat data/python_packages.csv | sed "s/,.*$//g" | grep -v "package")
+ipi install $(cat data/python_packages.csv | sed "s/,.*$//g" | grep -v "package")
 
 # setup vim environment
+echo "installing vim"
 ./apps/vim/deploy.sh
+echo "initializing vim package manager"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+echo "installing vim packages"
 vim -c ":PlugInstall | :qa"
-
-# setup alacritty terminal
-./apps/alacritty/deploy.sh
