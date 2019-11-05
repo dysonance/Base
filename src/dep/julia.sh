404502ec6824d3791209d9ba106e53cd7d7e84b8
Jacob Amos <jamos125@gmail.com>
2019-11-05 12:44:13 -0500

    jl dep script fix

--- src/dep/julia.sh
+++ src/dep/julia.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-unset DYLD_LIBRARY_PATH
-unset LD_LIBRARY_PATH
+REPO="https://github.com/JuliaLang/julia.git"
 
 cd $APPDIR
 if ! [ -d "Julia" ]; then mkdir Julia; fi
-if [ ! -d "src" ]; then git clone https://github.com/JuliaLang/julia.git src; fi
+cd Julia
+if ! [ -d "src" ]; then git clone $REPO src; fi
 cd src
+
 git checkout master
 git pull
 git checkout $(git tag | tail -n 1)
-
 if [ "$1" == "--clean" ]; then
     git clean -xfd
     make cleanall
 fi
 
 if [[ -z "${CPU}" ]]; then CPU=4; fi
+unset DYLD_LIBRARY_PATH
+unset LD_LIBRARY_PATH
 make -j $CPU
