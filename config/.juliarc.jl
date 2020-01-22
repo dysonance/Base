a1e8dd3fe43591a7b85a1fe7c9920d40fcd9ab41
Jacob Amos <jamos125@gmail.com>
2020-01-21 22:53:25 -0500

    julia repl config updates.

--- config/.juliarc.jl
+++ config/.juliarc.jl
@@ -1,12 +1,22 @@
-# julia custom startup file
-# to be placed at: ~/.julia/config/startup.jl
+#=
+julia custom startup file
+to be placed at: ~/.julia/config/startup.jl
 
-const CUSTOM_PROMPT = ":> "
+NOTE: this file is for julia versions v0.7 onward
+=#
 
 using REPL
-function customize_repl(repl)
+function setup_repl(repl)
     repl.interface = REPL.setup_interface(repl)
-    repl.interface.modes[1].prompt = CUSTOM_PROMPT
+    # default input prompt
+    repl.interface.modes[1].prompt = ":> "
+    # shell/terminal prompt
+    repl.interface.modes[2].prompt = "\$> "
+    # help prompt
+    repl.interface.modes[3].prompt = "?> "
+    #  # package manager prompt
+    #  repl.interface.modes[6].prompt = "[> "
     return
 end
-atreplinit(customize_repl)
+atreplinit(setup_repl)
+const global JULIA_HOME = expanduser("~/Applications/Julia/master/usr/bin")
