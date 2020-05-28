970a43e05c064d1655be62b25aa64d37dccbd32b
Jacob Amos <jamos125@gmail.com>
2020-05-28 13:21:31 -0400

    homebrew standard install as default

--- src/dep/brew.sh
+++ src/dep/brew.sh
@@ -2,25 +2,32 @@
 
 BASEDIR=$HOME/Base
 APPDIR=$HOME/Applications
-HOMEBREW_REPO="https://github.com/Homebrew/brew"
+if [ "$1" == "local" ]; then
+    # custom local/user directory environment
+    HOMEBREW_REPO="https://github.com/Homebrew/brew"
+    if ! [ -d "$APPDIR/Brew" ]; then mkdir $APPDIR/Brew; fi
+    cd $APPDIR/Brew
+    if ! [ -d "src" ]; then git clone $HOMEBREW_REPO src; fi
+    export PATH=$APPDIR/Brew/src/bin/brew:$PATH
+else
+    # default installation using command from https://brew.sh/
+    /bin/bash -c \
+        "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
 
-if ! [ -d "$APPDIR/Brew" ]; then mkdir $APPDIR/Brew; fi
-cd $APPDIR/Brew
-if ! [ -d "src" ]; then git clone $HOMEBREW_REPO src; fi
-cd src
-
-./bin/brew update
+# fetch current versions for brew and all packages
+brew update
 
 BREW_PACKAGES=$(cat $BASEDIR/data/packages/required/brew.csv)
 echo "===== BEGINNING BREW PACKAGE INSTALLATION ====="
-./bin/brew install htop tmux reattach-to-user-namespace  # prioritize useful pkgs w/ quick installs
-BREW_INSTALLED=$(./bin/brew list)
+brew install htop tmux reattach-to-user-namespace  # prioritize useful pkgs w/ quick installs
+BREW_INSTALLED=$(brew list)
 for pkg in $BREW_PACKAGES; do
-    if [ "$(./bin/brew list | grep $pkg)" == "" ]; then
+    if [ "$(brew list | grep $pkg)" == "" ]; then
         echo "== installation attempt beginning: $pkg =="
-        ./bin/brew install $pkg --verbose
+        brew install $pkg --verbose
         echo "== installation attempt finished: $pkg =="
-        BREW_INSTALLED=$(./bin/brew list)
+        BREW_INSTALLED=$(brew list)
     else
         echo "== skipping unnecessary installation: $pkg =="
     fi
